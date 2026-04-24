9b09003ca3991572582a370f87f2f70a6377cc9a
Nanditha <karnamnandithaa@gmail.com>
2026-04-24 21:33:52 +0530

    Initial commit

--- backend/main.java
+++ backend/main.java
@@ -1,15 +1,3 @@
-import java.util.*;
-class ClassName {
-
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        // ====== Start Coding Here ======
-         System.out.println("enter name");
-        String a = scn.nextLine();
-        
-        System.out.println("Welcome ");
-
-        // ====== End Coding ======
-        scn.close();
-    }
+public class main {
+    
 }
